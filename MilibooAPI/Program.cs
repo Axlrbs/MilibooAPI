3fef0655b90f4015c48790ac1f61b4edae7569b6
urashy <berkanakin05@gmail.com>
2025-03-25 10:39:33 +0100

    cors

--- MilibooAPI/Program.cs
+++ MilibooAPI/Program.cs
@@ -14,7 +14,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowVueApp", policy =>
     {
-        policy.WithOrigins("http://localhost:5173")
+        policy.WithOrigins("http://localhost:5173", "http://localhost:8978")
               .AllowAnyHeader()
               .AllowAnyMethod();
     });
