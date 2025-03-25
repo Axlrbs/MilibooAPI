397ecd34ce18ed21ba7cd91d53fa3080e4028613
axlrbs <axel.gerifaud14@yahoo.com>
2025-03-25 09:01:32 +0100

    projet test

---
+++ MilibooAPITests/Controllers/ProduitsControllerTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MilibooAPI.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MilibooAPI.Controllers.Tests
+{
+    [TestClass()]
+    public class ProduitsControllerTests
+    {
+        [TestMethod()]
+        public void ProduitsControllerTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
