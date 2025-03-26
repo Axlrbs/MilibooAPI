1177b082645e71e715bcdcfe84c345b41e1982c1
berthem1 <berthem1@A275-S19>
2025-03-26 17:38:49 +0100

    Changement ClientController pour essayer de post un client (RIEN NE MARCHE  mais aucune erreur qui empeche de lance l'API)

---
+++ MilibooAPI/Models/CreateClientDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace MilibooAPI.Models
+{
+    public class CreateClientDTO
+    {
+        public string PrenomPersonne { get; set; }
+        public string NomPersonne { get; set; }
+        public string TelPersonne { get; set; }
+        public string EmailClient { get; set; }
+        public string MdpClient { get; set; }
+    }
+}
